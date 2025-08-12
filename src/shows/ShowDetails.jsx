edfdd199f08fd61c8c53c0b4076657ef14f7fd81
Kevin Ong <kevinongg@gmail.comm>
2025-08-11 23:54:13 -0400

    finished

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,27 @@
 import "./shows.css";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
+import { useState } from "react";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+  if (!show) {
+    return (
+      <section className="show-details">
+        <p>Select a show to learn more</p>
+      </section>
+    );
+  }
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.title}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
